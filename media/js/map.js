262de4613e2a63e8801db7887546bc4d90a136f4
Ole Laursen <olau@iola.dk>
2011-08-29 11:55:23 +0200

    Remove debug

--- media/js/map.js
+++ media/js/map.js
@@ -6,7 +6,6 @@ $(function () {
         var fromProjection = new OpenLayers.Projection("EPSG:4326"); // transform from WGS 1984
         var toProjection = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
         map.addLayer(new OpenLayers.Layer.OSM());
-        console.log(+$(this).data("lon"), +$(this).data("lat"))
         map.setCenter(new OpenLayers.LonLat(+$(this).data("lon"), +$(this).data("lat")).transform(fromProjection, toProjection), 15);
     });
 });
